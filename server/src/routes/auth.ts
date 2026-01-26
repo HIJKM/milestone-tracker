9805fbf9541fe304021aa49d8c810433cedcf9a9
JHKIM <156522742+HIJKM@users.noreply.github.com>
2026-01-26 20:00:28 +0900

    Revert "lax -> none, etc.."
    This reverts commit 48a96161c50988a66e5b8b46aa948762bb12f194.

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -35,8 +35,8 @@ if (DEV_MODE) {
       // Save refresh token to httpOnly cookie
       res.cookie('refreshToken', refreshToken, {
         httpOnly: true,
-        secure: true,
-        sameSite: 'none',
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
       });
 
@@ -72,8 +72,8 @@ router.get(
     // Save refresh token to httpOnly cookie
     res.cookie('refreshToken', refreshToken, {
       httpOnly: true,
-      secure: true,
-      sameSite: 'none',
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'lax',
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
@@ -105,8 +105,8 @@ router.get(
     // Save refresh token to httpOnly cookie
     res.cookie('refreshToken', refreshToken, {
       httpOnly: true,
-      secure: true,
-      sameSite: 'none',
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'lax',
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
